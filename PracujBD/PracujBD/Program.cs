b7f61b9cda6fff61c07feea6b36e6343d79a3df4
kamilcyan <kamil.lucjanek@wp.pl>
2021-04-04 19:39:31 +0200

    Working with Service_user table

--- PracujBD/PracujBD/Program.cs
+++ PracujBD/PracujBD/Program.cs
@@ -15,20 +15,37 @@ namespace PracujBD
 
         public void WriteIn()
         {
-            string[] lines = System.IO.File.ReadAllLines("first-names.txt");
+            string[] firstnames = System.IO.File.ReadAllLines("first-names.txt");
+            string[] lastnames = System.IO.File.ReadAllLines("names.txt");
+            string[] adress = System.IO.File.ReadAllLines("places.txt");
+            string[] professions = System.IO.File.ReadAllLines("professions.txt");
+
+
             Random r = new Random();
 
             using (var db = new PracujContext())
             {
-                var name = new ServiceUser();
                 
                 for (int i = 1; i <= 10000; i++)
                 {
-                    int index = r.Next(0, lines.Length - 1);
-                    name.FirstName = lines[index];
+                    int indexFirstNames = r.Next(0, firstnames.Length - 1);
+                    int indexLastNames = r.Next(0, lastnames.Length - 1);
+                    int indexAdress = r.Next(0, adress.Length - 1);
+                    int indexProfessions = r.Next(0, professions.Length - 1);
+                    int phone = r.Next(5000000, 9999999);
+                    int expectedSalary = r.Next(20000, 200000);
+                    var name = new ServiceUser
+                    {
+                        FirstName = firstnames[indexFirstNames],
+                        LastName = lastnames[indexLastNames],
+                        Adress = adress[indexAdress],
+                        PnoneNumber = phone.ToString(),
+                        PnoneNumber = phone.ToString(),
+                        Profession = professions[indexProfessions]
+                    };
                     db.Add(name);
                     db.SaveChanges();
-                    db.Update(name);
+                    //db.Update(name);
                 }
 
             }
