a80e0242f184f6d445aebaa8bec821ff94bdb26d
kamilcyan <kamil.lucjanek@wp.pl>
2021-04-05 19:23:43 +0200

    Working with ServiceUser, adding Experience

--- PracujBD/PracujBD/Program.cs
+++ PracujBD/PracujBD/Program.cs
@@ -34,6 +34,7 @@ namespace PracujBD
                     int indexProfessions = r.Next(0, professions.Length - 1);
                     int phone = r.Next(5000000, 9999999);
                     int expectedSalary = r.Next(20000, 200000);
+                    int indexExperience = r.Next(0, professions.Length - 1);
                     var name = new ServiceUser
                     {
                         FirstName = firstnames[indexFirstNames],
@@ -41,16 +42,44 @@ namespace PracujBD
                         Adress = adress[indexAdress],
                         PnoneNumber = phone.ToString(),
                         ExpectedSalary = (decimal)expectedSalary,
-                        Profession = professions[indexProfessions]
+                        Profession = professions[indexProfessions],
+                        Experience = MakingExperience(professions[indexExperience]),
                     };
                     db.Add(name);
                     db.SaveChanges();
-                    //db.Update(name);
                 }
 
             }
 
 
+        }
+
+        public string MakingExperience(string xpr)
+        {
+            Random r = new Random();
+            Random r2 = new Random();
+
+            int duration = r.Next(0, 1500);
+            int duration2 = r.Next(0, 1500);
+
+            if(duration > duration2)
+            {
+                DateTime d1 = new DateTime(2015, 1, 1 );
+                string date1 = d1.AddDays(duration2).ToString("yyyy/MM/dd");
+                DateTime d2 = new DateTime(2015, 1, 1 );
+                string date2 = d2.AddDays(duration).ToString("yyyy/MM/dd");
+
+                return xpr + " " + date1 + "-" + date2;
+            }
+            else
+            {
+                DateTime d1 = new DateTime(2015, 1, 1);
+                string date1 = d1.AddDays(duration).ToString("yyyy/MM/dd");
+                DateTime d2 = new DateTime(2015, 1, 1);
+                string date2 = d2.AddDays(duration2).ToString("yyyy/MM/dd");
+
+                return xpr + " " + date1 + "-" + date2;
+            }
         }
     }
 }
